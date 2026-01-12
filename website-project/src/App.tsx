335346c5bf6621879c113b3f99928377f4291b85
Rusty Benson <rustybenson1985@gmail.com>
2026-01-12 17:53:46 -0600

    fix: changed path to load home page correctly

--- website-project/src/App.tsx
+++ website-project/src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import MainNavbar from './components/MainNavbar'
 import AlgebraPage from './pages/math/AlgebraPage'
 import ChemistryPage from './pages/science/ChemistryPage'
@@ -11,6 +11,7 @@ function App() {
       <MainNavbar />
       <Container>
         <Routes>
+          <Route path="/react-showcase" element={<Navigate replace to="/math/algebra" />} />
           <Route path="/math/algebra" element={<AlgebraPage />} />
           <Route path="/science/chemistry" element={<ChemistryPage />} />
         </Routes>
